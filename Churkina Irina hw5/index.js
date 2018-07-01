2a61122f0664c39f85bebfa338a1a840fa172f39
Ирина Чуркина <feel_good@MacBook-Pro-Irina.local>
2018-07-02 01:42:59 +0300

    fix current task

--- Churkina Irina hw5/index.js
+++ Churkina Irina hw5/index.js
@@ -1,140 +1,167 @@
 //general characteristics
-var foodItem = function(name, price, calories, portion) {
-    this.name = name;
-    this.price = price * portion;
-    this.calories = calories * portion;
+var FoodItem = function(item) {
+    this.name = item.name;
 }
 
-//salads:
-var salad = function(name, weight) {
-    //menu varants:
-    var _regularPortion = 100;
-    var _caesar = {
-        price: 100,
-        calories: 20
-    };
-    var _olivier = {
-        price: 50,
-        calories: 80
-    };
-
-    switch (name) {
-        case 'olivier':
-            foodItem.call(this, name, _olivier.price, _olivier.calories, weight / _regularPortion);
-            break;
-        case 'caesar':
-            foodItem.call(this, name, _caesar.price, _caesar.calories, weight / _regularPortion);
-            break;
-        default:
-            console.log('We don`t have this position in menu');
-            return
-            break;
-    }
-}
-salad.prototype = Object.create(foodItem.prototype);
-salad.prototype.varructor = salad;
-
-salad.prototype.getSize = function() {
-    return this.weight
+FoodItem.prototype.getItemPrice = function(){
+  return this.calculatePrice ? this.calculatePrice() : this.price;
 }
 
-//Hamburgers:
-var hamburger = function(name, bigSize, topping) {
-    if (!/burger/.test(name)) {
-        console.log('this is not a burger');
-        return
-    }
-    this.topping = [];
-    this.bigSize = bigSize;
-    var smallBurgerRatio = 0.5;
-    var _burger = {
-        price: 100,
-        calories: 40
-    }
-    if (this.bigSize) {
-        foodItem.call(this, name, _burger.price, _burger.calories, 1);
-    } else {
-        foodItem.call(this, name, _burger.price * smallBurgerRatio, _burger.calories * smallBurgerRatio, 1);
-    }
-    topping != undefined && topping.length ? this.addTopping(topping) : '';
+FoodItem.prototype.getItemCalories = function(){
+  return this.calculateCalories ? this.calculateCalories() : this.calories;
+}
+/**
+* Класс, объекты которого описывают параметры салата.
+*
+* @constructor
+* @param weight      Вес салата
+* @param name        Название салата
+*/
+
+var Salad = function(salad, weight) {
+    this.name = salad.name;
+    this.weight = weight;
+    this.price = salad.price;
+    this.calories = salad.calories;
+    FoodItem.apply(this, arguments);
 }
 
-hamburger.prototype = Object.create(foodItem.prototype);
-hamburger.prototype.varructor = hamburger;
 
-hamburger.prototype.addTopping = function(topping) {
-    var existed = this.topping.length;
-    var type = typeof(topping);
-    if (type === 'object') {
-        this.topping = this.topping.concat(topping);
-    } else if (type === 'string') {
-        !existed ? this.topping = [] : '';
-        this.topping.push(topping);
-    }
-    for (var t = existed; t < this.topping.length; t++) {
-        switch (this.topping[t]) {
-            case 'cheese':
-                this.calories += 20;
-                this.price += 10;
-                break;
-            case 'salad':
-                this.calories += 5;
-                this.price += 20;
-                break;
-            case 'potato':
-                this.calories += 20;
-                this.price += 10;
-                break;
-            default:
-                console.log('We don`t have this position in menu');
-                return
-                break;
-        }
-    }
+Salad.prototype = Object.create(FoodItem.prototype);
+Salad.prototype.constructor = Salad;
+
+Salad.CAESAR = {
+  name:'caesar',
+    price: 100,
+    calories: 20
+};
+
+Salad.OLIVIER = {
+  name:'olivier',
+    price: 50,
+    calories: 80
+};
+
+Salad.prototype.getName = function(){
+  return this.name;
 }
 
-hamburger.prototype.getSize = function() {
-    return this.bigSize ? 'big' : 'small'
+Salad.prototype.getWeight = function(){
+  return this.weight;
 }
 
-hamburger.prototype.getStuffing = function() {
-    return this.topping;
+Salad.prototype.getPrice = function(){
+  return this.price;
 }
+
+Salad.prototype.getCalories = function(){
+  return this.calories;
+}
+
+Salad.prototype.calculatePrice = function(){
+  return this.getPrice() * this.getWeight() / 100;
+}
+
+Salad.prototype.calculateCalories = function(){
+  return this.getCalories() * this.getWeight() / 100;
+}
+
+
+//new Salad(Salad.CAESAR, 200)
+
+/**
+* Класс, объекты которого описывают параметры гамбургера.
+*
+* @constructor
+* @param size        Размер
+* @param stuffing    Начинка
+*/
+
+var Hamburger = function(size, stuffing){
+  this.size = size;
+  this.stuffingList = [stuffing];
+  FoodItem.apply(this, arguments);
+}
+
+Hamburger.SIZE_SMALL = {name: 'smallBurger', price: 50, calories: 20};
+Hamburger.SIZE_LARGE = {name: 'bigBurger', price: 100, calories: 40};
+Hamburger.STUFFING_CHEESE = {name: 'cheese', price: 10, calories: 20};
+Hamburger.STUFFING_SALAD = {name: 'salad', price: 20, calories: 5};
+Hamburger.STUFFING_POTATO = {name: 'potato', price: 10, calories: 20};
+
+
+Hamburger.prototype = Object.create(FoodItem.prototype);
+Hamburger.prototype.constructor = Hamburger;
+
+Hamburger.prototype.getSize = function (){
+  return this.size;
+};
+
+Hamburger.prototype.getstuffing = function () {
+  return this.stuffingList;
+}
+
+Hamburger.prototype.calculatePrice = function () {
+  this.price = this.getSize().price;
+  var stuffingList = this.getstuffing();
+
+  for(var stuffingItem in stuffingList){
+    this.price += stuffingList[stuffingItem].price;
+  }
+  return this.price
+}
+
+Hamburger.prototype.calculateCalories = function (){
+  this.calories = this.getSize().calories;
+  var stuffingList = this.getstuffing();
+
+  for(var stuffingItem in stuffingList){
+    this.calories += stuffingList[stuffingItem].calories;
+  }
+  return this.calories;
+}
+
+Hamburger.prototype.addstuffing = function(stuffing) {
+    !this.stuffingList[stuffing] ? this.stuffingList[stuffing] = 1 : this.stuffingList[stuffing] += 1
+}
+
+/**
+* Класс, объекты которого описывают параметры напитка.
+*
+* @constructor
+* @param name       Название
+*/
 
 //drinks:
-var drink = function(name) {
-    //menu varants:
-    var _cola = {
-        price: 50,
-        calories: 40
-    }
-    var _coffee = {
-        price: 80,
-        calories: 20
-    }
-
-    switch (name) {
-        case 'cola':
-            foodItem.call(this, name, _cola.price, _cola.calories, 1);
-            break;
-        case 'coffee':
-            foodItem.call(this, name, _coffee.price, _coffee.calories, 1);
-            break;
-        default:
-            console.log('We don`t have this position in menu');
-            return
-            break;
-    }
+var Drink = function(drink) {
+  this.name = drink.name;
+  this.price = drink.price;
+  this.calories = drink.calories;
+  FoodItem.apply(this, arguments);
 }
 
-drink.prototype = Object.create(foodItem.prototype);
-drink.prototype.varructor = drink;
+Drink.prototype = Object.create(FoodItem.prototype);
+Drink.prototype.constructor = Drink;
 
-//order:
-var order = function(meals) {
-    let price;
-    let calories;
-    let self = this;
+Drink.COLA = {
+  name: 'cola',
+    price: 50,
+    calories: 40
+}
+
+Drink.COFFEE= {
+  name: 'coffee',
+    price: 80,
+    calories: 20
+}
+
+
+//Order:
+
+var Order = function(meals) {
+    var price;
+    var calories;
+    var self = this;
     this.itemsList = [];
     Array.isArray(meals) ? this.itemsList = meals : meals === undefined ? this.itemsList = [] : this.itemsList = [meals];
 
@@ -143,8 +170,8 @@ var order = function(meals) {
         calories = 0;
         if (self.itemsList.length) {
             self.itemsList.forEach(function(item) {
-                price += item.price;
-                calories += item.calories;
+                price += item.getItemPrice();
+                calories += item.getItemCalories();
             })
         }
     }
@@ -158,65 +185,49 @@ var order = function(meals) {
         return calories;
     }
     this.getOrderList = function() {
-        var orderNames = this.itemsList.map(function(item) {
+        var OrderNames = this.itemsList.map(function(item) {
             return item.name;
         });
-        return orderNames
+        return OrderNames
     }
 
     this.startCooking = function() {
-        self.addMeal = function() {
-            console.log('We`re preparing your order :) You cannot change it');
-            return
-        }
-        self.removeMeal = function() {
-            console.log('We`re preparing your order :) You cannot change it');
-            return
-        }
+        Object.freeze(self.itemsList);
     }
 }
 
-order.prototype.addMeal = function(meal) {
-    meal.name ? this.itemsList.push(meal) : '';
+Order.prototype.addMeal = function(meal) {
+    try{
+      meal.name ? this.itemsList.push(meal) : '';
+    }catch(err){
+      console.log('Error occurred. Probably your order is already cooking')
+    }
 }
-order.prototype.removeMeal = function(meal) {
-    if (typeof(meal) == 'string') {
-        for (var i = 0; i < this.itemsList.length; i++) {
-            if (this.itemsList[i].name == meal) {
-                this.itemsList.splice(i, 1);
-            }
-        }
-    } else if (this.itemsList.indexOf(meal) != -1) {
-        this.itemsList.splice(this.itemsList.indexOf(meal), 1);
+Order.prototype.removeMeal = function(meal) {
+    try{
+      if (typeof(meal) == 'string') {
+          for (var i = 0; i < this.itemsList.length; i++) {
+              if (this.itemsList[i].name == meal) {
+                  this.itemsList.splice(i, 1);
+              }
+          }
+      } else if (this.itemsList.indexOf(meal) != -1) {
+          this.itemsList.splice(this.itemsList.indexOf(meal), 1);
+      }
+    }catch(err){
+      console.log('Error occurred. Probably your order is already cooking')
     }
 }
 
 
 //create menu
 
-var caesar = new salad('caesar', 200); // name, weight
-//method witch is avaliable for salads: getSize();
+var caesar = new Salad(Salad.CAESAR, 200); // name, weight
 
-var burger = new hamburger('cheeseburger', true, ['potato', 'cheese']) //name, big=true|small=false, topping-string or topping array
-//methods witch are avaliable for hamburgers: getStaffing(); , getSize(); , addTopping(*string or an array*)
+var burger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_POTATO) //size, stuffing
 
-var cola = new drink('cola'); //name
+var cola = new Drink(Drink.COLA); //name
 
 
-//create new order:
-var order1 = new order([cola, burger]) //object or an array of objects
-console.log(order1.getOrderList());
-console.log(order1.getOrderPrice());
-console.log(order1.getOrderCalories());
-order1.addMeal(caesar);
-console.log(order1.getOrderList());
-order1.removeMeal(cola); //remove by clarifying object
-order1.removeMeal('cheeseburger'); //or an item`s name
-console.log(order1.getOrderList());
-order1.startCooking();
-
-var order2 = new order(new drink('coffee'));
-console.log(order2.getOrderList());
-order2.startCooking(); //cannot change order after cooking was started
-order2.addMeal(new salad('olivier'), 50);
-console.log(order2.getOrderList());
+//create new Order:
+var order1 = new Order([cola, burger]) //object or an array of objects
